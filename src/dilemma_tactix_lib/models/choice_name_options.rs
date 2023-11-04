d61481ff7980026d4337319fda426fa7204c5ca6
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-11-04 17:51:44 -0400

    docs: updated docs for GameChoiceOptions

--- src/dilemma_tactix_lib/models/choice_name_options.rs
+++ src/dilemma_tactix_lib/models/choice_name_options.rs
@@ -43,7 +43,82 @@
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
 use rand::{self, Rng};
-
+/// A data struct that holds the names of the choices.
+///
+/// This is a simple data struct that is used to internally store
+/// the names of choice pairs that can be given for a single game.
+///
+/// The names are taken from the [Universal Paperclips](https://www.decisionproblem.com/paperclips/index2.html)
+/// game, and are used to make the game more interesting.
+///
+/// The names are stored in two arrays, one for each choice. The
+/// names are then accessed by index, and returned as a pair. The table
+/// of all choices and their respective indices can be found [below](#available-options).
+///
+/// The struct also provides a way to get a random pair of names.
+///
+/// # Examples
+///
+/// ## Get a specific pair
+///
+/// ```
+/// use dilemma_tactix_lib::models::ChoiceNameOptions;
+///
+/// let choice_name_options = ChoiceNameOptions::new();
+/// let (choice_a, choice_b) = choice_name_options.get_choice_pair(0);
+///
+/// assert_eq!(choice_a, "cooperate");
+/// assert_eq!(choice_b, "defect");
+/// ```
+///
+/// ## Get a random pair
+/// ```
+/// use dilemma_tactix_lib::models::ChoiceNameOptions;
+///
+/// let choice_name_options = ChoiceNameOptions::new();
+/// let (choice_a, choice_b) = choice_name_options.get_random_pair();
+/// assert_ne!(choice_a, choice_b);
+/// ```
+///
+/// # Notes
+///
+/// The names are taken from the [Universal Paperclips](https://www.decisionproblem.com/paperclips/index2.html).
+///
+/// I have also decided to make a static array that can be stored in the binary itself to make it easier to
+/// distribute the game. This means that the names are not configurable, and are the same for every game.
+///
+/// For this reason, you would usually want to use the `get_random_pair` function, as it will return a random
+/// pair of names.
+///
+/// # Available Options
+///
+/// | Index | Choice Aleph | Choice Bey |
+/// | ----- | -------- | -------- |
+/// | 0     | cooperate | defect |
+/// | 1     | swerve | straight |
+/// | 2     | macro | micro |
+/// | 3     | fight | back_down |
+/// | 4     | bet | fold |
+/// | 5     | raise_price | lower_price |
+/// | 6     | opera | football |
+/// | 7     | go | stay |
+/// | 8     | heads | tails |
+/// | 9     | particle | wave |
+/// | 10    | discrete | continuous |
+/// | 11    | peace | war |
+/// | 12    | search | evaluate |
+/// | 13    | lead | follow |
+/// | 14    | accept | reject |
+/// | 15    | accept | deny |
+/// | 16    | attack | decay |
+///
+/// # See Also
+///
+/// * [Universal Paperclips](https://www.decisionproblem.com/paperclips/index2.html)
+/// * [get_random_pair](ChoiceNameOptions::get_random_pair)
+/// * [get_choice_pair](ChoiceNameOptions::get_choice_pair)
+#[derive(Debug, Clone, Copy)]
+#[allow(clippy::doc_markdown)]
 pub struct ChoiceNameOptions {
     choice_a_options: [&'static str; 17],
     choice_b_options: [&'static str; 17],
@@ -51,6 +126,33 @@ pub struct ChoiceNameOptions {
 }
 
 impl ChoiceNameOptions {
+    /// Create a new `ChoiceNameOptions` struct.
+    ///
+    /// This function creates a new `ChoiceNameOptions` struct, and
+    /// can then be used to generate a random name pair or extract a specific
+    /// name pair.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use dilemma_tactix_lib::models::ChoiceNameOptions;
+    ///
+    /// let choice_name_options = ChoiceNameOptions::new();
+    ///
+    /// let (choice_a, choice_b) = choice_name_options.get_choice_pair(0);
+    ///
+    /// assert_eq!(choice_a, "cooperate");
+    /// assert_eq!(choice_b, "defect");
+    /// ```
+    ///
+    /// # Returns
+    ///
+    /// A new `ChoiceNameOptions` struct.
+    ///
+    /// # See Also
+    ///
+    /// * [`get_random_pair`](ChoiceNameOptions::get_random_pair)
+    /// * [`get_choice_pair`](ChoiceNameOptions::get_choice_pair)
     #[must_use]
     pub const fn new() -> Self {
         let choice_aleph_options = [
@@ -99,11 +201,77 @@ impl ChoiceNameOptions {
         }
     }
 
+    /// Get a specific choice pair.
+    ///
+    /// This function returns a specific choice pair, based on the index
+    /// provided. The index must be less than the length of the array.
+    ///
+    /// # Arguments
+    ///
+    /// * `n` - The index of the choice pair to return.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use dilemma_tactix_lib::models::ChoiceNameOptions;
+    ///
+    /// let choice_name_options = ChoiceNameOptions::new();
+    ///
+    /// let (choice_a, choice_b) = choice_name_options.get_choice_pair(0);
+    ///
+    /// assert_eq!(choice_a, "cooperate");
+    /// assert_eq!(choice_b, "defect");
+    /// ```
+    ///
+    /// # Returns
+    ///
+    /// A tuple containing the two choices.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if the index is greater than the length of the array.
+    ///
+    /// # See Also
+    ///
+    /// * [`get_random_pair`](ChoiceNameOptions::get_random_pair)
     #[must_use]
     pub const fn get_choice_pair(&self, n: usize) -> (&'static str, &'static str) {
+        assert!(n < self.length, "Index out of bounds.");
         (self.choice_a_options[n], self.choice_b_options[n])
     }
 
+    /// Get a random choice pair.
+    ///
+    /// This function returns a random choice pair, based on the length
+    /// of the array.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use dilemma_tactix_lib::models::ChoiceNameOptions;
+    ///
+    /// let choice_name_options = ChoiceNameOptions::new();
+    ///
+    /// let (choice_a, choice_b) = choice_name_options.get_random_pair();
+    /// assert_ne!(choice_a, choice_b);
+    /// ```
+    ///
+    /// # Returns
+    ///
+    /// A tuple containing the two choices.
+    ///
+    /// # See Also
+    ///
+    /// * [`get_choice_pair`](ChoiceNameOptions::get_choice_pair)
+    ///
+    /// # Notes
+    ///
+    /// This function uses the [`rand`](https://crates.io/crates/rand) crate to generate a random number.
+    ///
+    /// # See Also
+    ///
+    /// * [`rand`](https://crates.io/crates/rand)
+    /// * [`get_choice_pair`](ChoiceNameOptions::get_choice_pair)
     #[must_use]
     pub fn get_random_pair(&self) -> (&'static str, &'static str) {
         let mut rng = rand::thread_rng();
