721149c2bab6ee7d0dac32d4d8ea3d4a6d1cf0a9
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-01-30 11:52:29 -0500

    feat(Choice): add the choice enum

--- src/dilemma_tactix_lib/models/mod.rs
+++ src/dilemma_tactix_lib/models/mod.rs
@@ -48,12 +48,14 @@
 // * SOFTWARE.
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
+mod choice;
 mod choice_name_options;
 mod game_grid;
 mod game_option_builder;
 mod game_options;
 mod number_pair;
 
+pub use choice::Choice;
 pub use choice_name_options::ChoiceNameOptions;
 pub use game_grid::GameGrid;
 pub use game_option_builder::GameOptionsBuilder;
