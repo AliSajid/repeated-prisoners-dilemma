4b00144754168856418a2d3e36e3a04d8dbab172
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-02-03 22:18:53 -0500

    feat: implement a simple TUI for the game
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/tactix_tui/main.rs
+++ src/tactix_tui/main.rs
@@ -42,6 +42,87 @@
 // * SOFTWARE.
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-pub fn main() {
-    println!("Hello, world!");
+use std::{
+    error::Error,
+    io,
+};
+
+use crossterm::{
+    event::{
+        self,
+        DisableMouseCapture,
+        EnableMouseCapture,
+        Event,
+        KeyCode,
+    },
+    execute,
+    terminal::{
+        disable_raw_mode,
+        enable_raw_mode,
+        EnterAlternateScreen,
+        LeaveAlternateScreen,
+    },
+};
+use ratatui::{
+    backend::{
+        Backend,
+        CrosstermBackend,
+    },
+    Terminal,
+};
+
+mod app;
+mod ui;
+use crate::{
+    app::App,
+    ui::ui,
+};
+
+fn main() -> Result<(), Box<dyn Error>> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stderr = io::stderr(); // This is a special case. Normally using stdout is fine
+    execute!(stderr, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stderr);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let mut app = App::new();
+    let _res = run_app(&mut terminal, &mut app);
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, app: &mut App) -> io::Result<bool> {
+    loop {
+        terminal.draw(|f| ui(f, app))?;
+
+        if let Event::Key(key) = event::read()? {
+            if key.kind == event::KeyEventKind::Release {
+                // Skip events that are not KeyEventKind::Press
+                continue;
+            }
+            match key.code {
+                KeyCode::Char('q') => {
+                    eprint!("'q' pressed; breaking loop");
+                    break;
+                }
+                KeyCode::Esc => {
+                    eprint!("ESC pressed; breaking loop");
+                    break;
+                }
+                _ => {}
+            }
+        }
+    }
+    Ok(false)
 }
