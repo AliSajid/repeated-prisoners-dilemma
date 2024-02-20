eba7f9939aae97551c4d7d5f607248f53d89185f
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-02-20 18:58:03 -0500

    feat: add completed table interface
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/tactix_tui/ui.rs
+++ src/tactix_tui/ui.rs
@@ -89,9 +89,10 @@ pub fn ui(frame: &mut Frame, app: &App) {
         .direction(Direction::Vertical)
         .constraints([
             Constraint::Length(5),
-            Constraint::Min(0),
+            Constraint::Fill(1),
             Constraint::Length(3),
         ])
+        .flex(layout::Flex::Center)
         .split(size);
 
     render_header(frame, screen_layout[0]);
@@ -103,6 +104,7 @@ pub fn ui(frame: &mut Frame, app: &App) {
             Constraint::Ratio(3, 5),
             Constraint::Ratio(1, 5),
         ])
+        .flex(layout::Flex::Center)
         .split(screen_layout[1]);
 
     let game_rows = Layout::default()
@@ -113,75 +115,49 @@ pub fn ui(frame: &mut Frame, app: &App) {
     let beth_header_row = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([
-            Constraint::Ratio(1, 4),
-            Constraint::Ratio(2, 4),
-            Constraint::Ratio(1, 4),
+            Constraint::Ratio(1, 5),
+            Constraint::Ratio(1, 5),
+            Constraint::Ratio(2, 5),
+            Constraint::Ratio(1, 5),
         ])
         .split(game_rows[1]);
 
     let aleph_header_row = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Ratio(1, 4),
-            Constraint::Ratio(2, 4),
-            Constraint::Ratio(1, 4),
-        ])
+        .constraints([Constraint::Percentage(20); 5])
         .split(game_rows[2]);
 
-    let aleph_strategy_rect = Layout::default()
+    let atlantis_row = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([Constraint::Ratio(1, 2); 2])
-        .split(aleph_header_row[1]);
-
-    let atlantis_header_row = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Ratio(1, 4),
-            Constraint::Ratio(2, 4),
-            Constraint::Ratio(1, 4),
-        ])
+        .constraints([Constraint::Percentage(20); 5])
         .split(game_rows[3]);
 
-    let olympus_header_row = Layout::default()
+    let olympus_row = Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([
-            Constraint::Ratio(1, 4),
-            Constraint::Ratio(2, 4),
-            Constraint::Ratio(1, 4),
-        ])
+        .constraints([Constraint::Percentage(20); 5])
         .split(game_rows[4]);
-
-    let atlantis_score_rect = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Ratio(1, 2); 2])
-        .split(atlantis_header_row[1]);
-
-    let olympus_score_rect = Layout::default()
-        .direction(Direction::Horizontal)
-        .constraints([Constraint::Ratio(1, 2); 2])
-        .split(olympus_header_row[1]);
 
     let beth_text = Paragraph::new("Player Beth")
         .block(Block::default().title("").borders(Borders::ALL))
         .style(Style::new().white().on_black())
         .alignment(Alignment::Center);
 
-    frame.render_widget(beth_text, beth_header_row[1]);
+    frame.render_widget(beth_text, beth_header_row[2]);
 
     let aleph_text = Paragraph::new("Player Aleph")
         .block(Block::default().title("").borders(Borders::ALL))
         .style(Style::new().white().on_black())
         .alignment(Alignment::Center);
 
-    frame.render_widget(aleph_text, aleph_header_row[0]);
+    frame.render_widget(aleph_text, aleph_header_row[1]);
 
     let atlantis_text = Paragraph::new(app.game_grid.game_options.choice_atlantis().to_string())
         .block(Block::default().title("").borders(Borders::ALL))
         .style(Style::new().white().on_black())
         .alignment(Alignment::Center);
 
-    frame.render_widget(atlantis_text.clone(), atlantis_header_row[0]);
-    frame.render_widget(atlantis_text.clone(), aleph_strategy_rect[0]);
+    frame.render_widget(atlantis_text.clone(), aleph_header_row[2]);
+    frame.render_widget(atlantis_text.clone(), atlantis_row[1]);
 
     let atlantis_atlantis_score =
         Paragraph::new(app.game_grid.game_options.atlantis_atlantis().to_string())
@@ -189,7 +165,7 @@ pub fn ui(frame: &mut Frame, app: &App) {
             .style(Style::new().white().on_black())
             .alignment(Alignment::Center);
 
-    frame.render_widget(atlantis_atlantis_score.clone(), atlantis_score_rect[0]);
+    frame.render_widget(atlantis_atlantis_score, atlantis_row[2]);
 
     let atlantis_olympus_score =
         Paragraph::new(app.game_grid.game_options.atlantis_olympus().to_string())
@@ -197,15 +173,15 @@ pub fn ui(frame: &mut Frame, app: &App) {
             .style(Style::new().white().on_black())
             .alignment(Alignment::Center);
 
-    frame.render_widget(atlantis_olympus_score.clone(), atlantis_score_rect[1]);
+    frame.render_widget(atlantis_olympus_score, atlantis_row[3]);
 
     let olympus_text = Paragraph::new(app.game_grid.game_options.choice_olympus().to_string())
         .block(Block::default().title("").borders(Borders::ALL))
         .style(Style::new().white().on_black())
         .alignment(Alignment::Center);
 
-    frame.render_widget(olympus_text.clone(), olympus_header_row[0]);
-    frame.render_widget(olympus_text.clone(), aleph_strategy_rect[1]);
+    frame.render_widget(olympus_text.clone(), aleph_header_row[3]);
+    frame.render_widget(olympus_text.clone(), olympus_row[1]);
 
     let olympus_atlantis_score =
         Paragraph::new(app.game_grid.game_options.olympus_atlantis().to_string())
@@ -213,7 +189,7 @@ pub fn ui(frame: &mut Frame, app: &App) {
             .style(Style::new().white().on_black())
             .alignment(Alignment::Center);
 
-    frame.render_widget(olympus_atlantis_score.clone(), olympus_score_rect[0]);
+    frame.render_widget(olympus_atlantis_score, olympus_row[2]);
 
     let olympus_olympus_score =
         Paragraph::new(app.game_grid.game_options.olympus_olympus().to_string())
@@ -221,7 +197,7 @@ pub fn ui(frame: &mut Frame, app: &App) {
             .style(Style::new().white().on_black())
             .alignment(Alignment::Center);
 
-    frame.render_widget(olympus_olympus_score.clone(), olympus_score_rect[1]);
+    frame.render_widget(olympus_olympus_score, olympus_row[3]);
 
-    render_footer(frame, screen_layout[2])
+    render_footer(frame, screen_layout[2]);
 }
